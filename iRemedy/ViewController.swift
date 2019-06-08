5806c23e6be80aff75928a4b8e04d89d41c0f946
Joao Raffs <j.f.raffs@gmail.com>
2019-06-08 16:01:18 -0300

    Initial Commit

---
+++ iRemedy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iRemedy
+//
+//  Created by João Raffs on 08/06/19.
+//  Copyright © 2019 Joao Raffs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
